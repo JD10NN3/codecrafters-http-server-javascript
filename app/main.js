fde8e5781e7df62b67b23aa22084ad1f763586e4
Jonathan Dionne <jonathandionne@me.com>
2024-07-09 21:45:30 -0400

    echo path

--- app/main.js
+++ app/main.js
@@ -3,6 +3,9 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+// the allowed paths in regex
+const allowedPaths = ["^\\/$", "^\\/echo\\/(.*)$"];
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
 
@@ -12,11 +15,24 @@ const server = net.createServer((socket) => {
         const [method, path, protocol] = lines[0].split(" ");
         console.log("Path:", path);
 
-        if (path !== "/") {
+        // check if the path is allowed
+        if (!allowedPaths.some(p => new RegExp(p).test(path))) {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        } else {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+            socket.end();
+            return;
         }
+            
+        // check if the path is echo
+        const echoMatch = path.match(new RegExp(allowedPaths[1]));
+        if (echoMatch) {
+            socket.write(`HTTP/1.1 200 OK\r\n\r\n${echoMatch[1]}`);
+            socket.end();
+            return;
+        }
+
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        socket.end();
+        
     });
 
     socket.on("close", () => {
